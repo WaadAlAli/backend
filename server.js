63d9ef2d25526e75b10f7561d945af3dccd69cb6
Waad <waadelali@gmail.com>
2026-01-02 23:39:43 +0200

    Update commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use("/images", express.static("images"));
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
 });
