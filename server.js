437800369333290067f56486564b91d5cb989b7d
Waad <waadelali@gmail.com>
2026-01-07 23:00:15 +0200

    Update commit

--- server.js
+++ server.js
@@ -6,12 +6,7 @@ const multer = require("multer");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
-require("dotenv").config();
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASS:", process.env.DB_PASS ? "SET" : "NOT SET");
-console.log("DB_NAME:", process.env.DB_NAME);
-console.log("DB_PORT:", process.env.DB_PORT);
+
 
 app.use(cors());
 app.use(express.json());
